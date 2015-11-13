61281da5fd57136d0626f40e5a1ca5185f88512b
Ygor Máximo <maximozsec@protonmail.ch>
2015-11-13 15:21:27 -0300

    added compatibility with Arch linux (distro detection); added symlink

--- setup/install.sh
+++ setup/install.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-apt-get install python-pip
+# Detecting distro
 
-#ubuntu 14.04 LTS dependencies
-apt-get install python-dev
-apt-get install python-m2crypto
-apt-get install swig
-pip install pycrypto
+#Arch Linux
+if [ -e /etc/pacman.d ]
+then
+	# dependencies
+	sudo pacman -S swig python2-pip python2-m2crypto python2-crypto python2-iptools
 
-#kali dependencies
-pip install iptools
-pip install pydispatcher
+	sudo pip2 install pydispatcher 
 
-./setup_database.py
+	# replacing strings in reset.sh to use python2
+	sed -i 's/.\/setup_database.py/python2 setup_database.py/g' reset.sh
+	sed -i 's/.\/empire/empire/g' reset.sh
+
+	#Configure database
+	python2 setup_database.py
+
+#Debian-like
+elif [ -e /etc/apt ]
+then
+	# dependencies
+	sudo apt-get install python-pip python-dev python-m2crypto swig
+
+	pip install pycrypto iptools pydispatcher
+	
+	#Configure database
+	./setup_database.py
+fi
+
+sudo mv ../../Empire ../../empire
+sudo mv ../../empire /usr/share
+
+# creating a symlink
+sudo sh -c "echo \#\!/bin/bash >> /usr/bin/empire"
+sudo sh -c "echo cd /usr/share/empire >> /usr/bin/empire"
+sudo sh -c "echo exec python2 empire \$\@\ >> /usr/bin/empire"
+
+sudo chmod +x /usr/bin/empire
